863e7f8ee3f835194e5d67bd4f8ffa50e09a0098
comercialtalleresunai-web <comercialtalleresunai@gmail.com>
2026-06-29 10:48:17 +0200

    Fix: eliminar caracter 'h' extra en config.js que causaba SyntaxError

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ const CONFIG = {
   SUPABASE_URL: 'https://jlwiikdwlsqwcaoxlljg.supabase.co',
 
   // anon public key (segura para el frontend)
-  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impsd2lpa2R3bHNxd2Nhb3hsbGpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0NzAzMDAsImV4cCI6MjA5ODA0NjMwMH0.c-Qkr9W_C4fh9vbZySnrGVc2XOJtijI4uYj8_YIOg7c',h
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impsd2lpa2R3bHNxd2Nhb3hsbGpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0NzAzMDAsImV4cCI6MjA5ODA0NjMwMH0.c-Qkr9W_C4fh9vbZySnrGVc2XOJtijI4uYj8_YIOg7c',
 
   // Nombre de la empresa (aparece en la cabecera)
   EMPRESA: 'Suministros Talleres Unai SL',
